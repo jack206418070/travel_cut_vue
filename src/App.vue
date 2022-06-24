05ccd85e127f8fd0b76a44e257ff828c2540d3a7
GinJack <jack90928@gmail.com>
2022-06-24 19:30:41 +0800

    color select

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="outer" :class="is_theme">
+    <div class="select-color" style="position: fixed; top: 0; right: 0; z-index: 9999; width: 150px;">
+      <select name="color" id="color" v-model="is_theme" style="width: 100%; padding: 8px;">
+        <option value="theme1" selected>主題色一</option>
+        <option value="theme2">主題色二</option>
+        <option value="theme3">主題色三</option>
+        <option value="theme4">主題色四</option>
+        <option value="theme5">主題色五</option>
+      </select>
+    </div>
     <router-view :is_theme = 'is_theme' />
   </div>
 </template>
